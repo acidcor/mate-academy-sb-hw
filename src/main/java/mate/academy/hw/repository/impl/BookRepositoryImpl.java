1222f47a5004f15c24e7b8677de7c8fd1e2a9eef
acor <deadfoxua1337@gmail.com>
2026-07-11 16:09:15 +0200

    Style fix

--- src/main/java/mate/academy/hw/repository/impl/BookRepositoryImpl.java
+++ src/main/java/mate/academy/hw/repository/impl/BookRepositoryImpl.java
@@ -1,17 +1,13 @@
 package mate.academy.hw.repository.impl;
 
 import exceptrion.DataProcessingException;
-import jakarta.persistence.criteria.CriteriaQuery;
 import java.util.List;
-import java.util.Queue;
-
 import lombok.RequiredArgsConstructor;
 import mate.academy.hw.model.Book;
 import mate.academy.hw.repository.BookRepository;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
