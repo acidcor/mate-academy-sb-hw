518db9d9ca4eb665e693b238ac52f3ef2ff38a96
acor <deadfoxua1337@gmail.com>
2026-07-13 15:43:31 +0200

    Delete: @ResponseBody annotation in BookController

--- src/main/java/mate/academy/hw/controller/BookController.java
+++ src/main/java/mate/academy/hw/controller/BookController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -20,13 +19,11 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    @ResponseBody
     public List<BookDto> getAll() {
         return bookService.findAll();
     }
 
     @GetMapping("{id}")
-    @ResponseBody
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
     }
